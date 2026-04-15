46c05806dc026623277d68a71dfd0f9cddf3e0bd
gaozw <1215629344@qq.com>
2026-04-15 23:17:51 +0800

    FlyerDrawCloudAi追加添加文本功能，优化其他模块代码

--- src/components/common/FlyerMakeRecord.vue
+++ src/components/common/FlyerMakeRecord.vue
@@ -1,19 +1,17 @@
 <template>
-  <div style="width:100%;height:100%; padding:10px;display: flex; flex-direction: column;flex-wrap: wrap;align-content: center;align-items: center;gap: 10px;">
-    <div>
+  <div style="width:100%;height:100%;display: flex; flex-direction: column;flex-wrap: wrap;align-content: center;align-items: center;gap: 10px;">
+<!--    <div>
       <a-progress type="circle" :percent="currentTime*100/maxTime" :format="percent => formattedTime" />
-    </div>
+    </div>-->
 
-    <div style="display: flex; flex-direction: row;flex-wrap: wrap;align-content: center;align-items: center;gap: 10px">
+<!--    <div style="display: flex; flex-direction: row;flex-wrap: wrap;align-content: center;align-items: center;gap: 10px">
       <a-button style="" type="primary" @click="startRecording" :disabled="isRecording">开始录音</a-button>
       <a-button style=""  type="primary" @click="stopRecording" :disabled="!isRecording">停止录音</a-button>
       <a-button @click="visible = true" >录音记录</a-button>
-    </div>
-    <div style="width: 80%;height: 50%;display: block">
-      <video style="width: 1000px;height: 100%" id="preview" autoplay muted playsinline></video>
-    </div>
+    </div>-->
+    <video style="width: 1000px;height: 100%" id="preview" autoplay muted playsinline></video>
   </div>
-  <a-drawer
+  <a-drawer :width="720"
       v-model:open="visible"
       class="custom-class"
       title="录音记录"
@@ -21,8 +19,9 @@
   >
     <div v-for="(rec, index) in recordings.slice().reverse()">
       <figure>
-        <figcaption><a href="javascript:" @click="downloadRecording(rec.blob, index)">录音 {{ index + 1 }}</a></figcaption>
+        <figcaption><a href="javascript:" @click="downloadRecording(rec.blob, index)">{{ rec.name }}</a></figcaption>
         <figcaption>{{rec.time}}</figcaption>
+        <figcaption>{{rec.meta.url + " from " + rec.meta.start + ' to ' + rec.meta.end}}</figcaption>
         <audio :src="rec.url" :controls="true" style="width: 90%"></audio>
       </figure>
     </div>
@@ -30,7 +29,7 @@
 </template>
 
 <script setup>
-import {ref, computed, onUnmounted, watch} from 'vue'
+import {ref, computed, onUnmounted, watch, reactive} from 'vue'
 import {message} from "ant-design-vue";
 
 const visible = ref(false)
@@ -45,7 +44,7 @@ const maxTime = ref(20)
 
 // 录音列表
 const recordings = ref([])
-
+const emits = defineEmits(["upload"])
 // 格式化时间
 const formattedTime = computed(() => {
   const minutes = Math.floor(currentTime.value / 60)
@@ -83,17 +82,19 @@ const startRecording = async () => {
       const audioBlob = new Blob(audioChunks.value, { type: 'video/webm' })
       const audioUrl = URL.createObjectURL(audioBlob)
       const timestamp = new Date().toLocaleTimeString()
-
-      recordings.value.push({
+      const r = {
         url: audioUrl,
+        name:`record_${Date.now()}.webm`,
         blob: audioBlob,
         time: timestamp,
-        duration: formattedTime.value
-      })
-
+        duration: formattedTime.value,
+        meta: JSON.parse(JSON.stringify(meta))
+      }
+      recordings.value.push(r)
+      emits("upload", r)
       // 释放麦克风
       stream.getTracks().forEach(track => track.stop())
-      visible.value = true
+
     }
 
     mediaRecorder.value.start()
@@ -107,17 +108,40 @@ const startRecording = async () => {
     message.warn('无法访问麦克风，请检查权限')
   }
 }
-
 // 停止录音
-const stopRecording = () => {
+const stopRecording = (flag) => {
   if (mediaRecorder.value && mediaRecorder.value.state === 'recording') {
     mediaRecorder.value.stop()
     clearInterval(timerInterval)
     isRecording.value = false
     currentTime.value = 0;
+    if (!flag) {
+      visible.value = true
+    }
   }
 }
 
+const showNotes = () => {
+  visible.value = true;
+}
+const updateMaxTime = (max) => {
+  maxTime.value = max
+}
+const meta = reactive({
+  url:'',
+  vid:'',
+  start:'00:00:00',
+  end:'00:00:00',
+})
+const setRecordMeta = (data) => {
+  meta.url = data.url
+  meta.start = data.start
+  meta.end = data.end
+}
+const getRecording = () => {
+
+}
+defineExpose({startRecording, stopRecording,showNotes,updateMaxTime,setRecordMeta})
 // 下载录音
 const downloadRecording = (blob, index) => {
   const url = URL.createObjectURL(blob)
--- src/components/player/FlyerBestPlayer.vue
+++ src/components/player/FlyerBestPlayer.vue
@@ -1,8 +1,9 @@
 <script setup>
-import {onMounted, ref} from "vue";
+import {onMounted, reactive, ref} from "vue";
 import {aget, apost} from "../../utils/Http.js";
 import {message} from "ant-design-vue";
 import FlyerPlayer from "../common/FlyerPlayer.vue";
+import FlyerMakeRecord from "../common/FlyerMakeRecord.vue";
 const dataList = ref([])
 
 const queryList = (call) => {
@@ -105,6 +106,7 @@ const markEnd = () => {
   endValue.value = formatTime(flyerPlayerRef.value.currentTime())
 }
 const editVideoList = ref([])
+const recordVideoList = ref([])
 
 const queryEditVideoList = (vid) => {
   aget('/player/edit/list?vid='+vid, (res) => {
@@ -118,13 +120,63 @@ const apiPref = import.meta.env.VITE_APP_API_URL
 const editName = ref('')
 
 const flyerPlayerRef = ref()
+const flyerMakeRecordRef = ref()
+const startRecordFlag = ref(false)
+const recordVisible = ref(false)
 const selectToPlay = (vid) => {
   const data = dataList.value.find(item => item.vid === vid)
   if (!!!data) {
     return
   }
-  const url = import.meta.env.VITE_APP_API_URL + data.url
+  const url = apiPref + data.url
   flyerPlayerRef.value.play(vid,url)
+}
+const recordMeta = reactive({
+  url:'',
+  start:'',
+  end:'',
+})
+const startRecord = () => {
+  const data = dataList.value.find(item => item.vid === movieItem.value)
+  recordMeta.url = data.url
+  recordMeta.start = formatTime(flyerPlayerRef.value.currentTime())
+  flyerMakeRecordRef.value.updateMaxTime(60 * 60)
+  flyerMakeRecordRef.value.startRecording()
+  startRecordFlag.value = true;
+}
+const uploadRecord = () => {
+
+}
+const stopRecord = () => {
+  recordMeta.end = formatTime(flyerPlayerRef.value.currentTime())
+  flyerMakeRecordRef.value.setRecordMeta(recordMeta)
+  flyerMakeRecordRef.value.stopRecording(true)
+  flyerPlayerRef.value.stop();
+  startRecordFlag.value = false;
+}
+const showRecord = () => {
+  recordVisible.value = true
+}
+const mergeRecord = () => {
+
+}
+const recordUpload = async (r) => {
+  const formData = new FormData();
+  formData.append('record', r.blob, r.name); // 第三个参数是文件名
+  formData.append('video_path', r.meta.url)
+  formData.append('video_start', r.meta.start)
+  formData.append('video_end', r.meta.end)
+  try {
+    const response = await fetch( apiPref + '/player/record/merge', {
+      method: 'POST',
+      body: formData
+    });
+
+    const result = await response.json();
+    console.log('上传成功', result);
+  } catch (error) {
+    console.error('上传失败', error);
+  }
 }
 </script>
 
@@ -133,9 +185,15 @@ const selectToPlay = (vid) => {
   <a-select v-model:value="movieItem" @change="(vid) => {selectToPlay(vid);}" :style="{width: widthRate + '%',marginBottom: '10px'}" placeholder="选择聚集">
     <a-select-option v-for="item in dataList" :value="item.vid">{{ item.name }}</a-select-option>
   </a-select>
-  <div style="background: black;height:calc(69% - 60px);" :style="{width: widthRate + '%'}" >
-    <FlyerPlayer ref="flyerPlayerRef"></FlyerPlayer>
+  <div style="display: flex; flex-direction: row;flex-wrap: wrap;align-items: center;">
+    <div style="background: black;width:800px;height:600px;" >
+      <FlyerPlayer ref="flyerPlayerRef"></FlyerPlayer>
+    </div>
+    <div v-show="startRecordFlag" style="background: black;width:800px;height:600px;" >
+      <FlyerMakeRecord ref="flyerMakeRecordRef" @upload="recordUpload"></FlyerMakeRecord>
+    </div>
   </div>
+
 <!--  <video ref="videoRef" @loadedmetadata="resumePlayTime" :controls="controls" style="background: black;height:calc(69% - 60px);" :style="{width: widthRate + '%'}"  @mouseover="()=> controls = true" @mouseleave="()=> controls = false">-->
 <!--  </video>-->
   <div :style="{width: widthRate + '%',marginTop: '10px'}">
@@ -143,20 +201,45 @@ const selectToPlay = (vid) => {
     <a-input v-model:value="startValue" style="width: calc(10% - 10px);margin-right:10px;"></a-input>
     <a-button type="primary" :style="{width: 'calc(10% - 10px)',marginRight: '10px'}" @click="markEnd">标记终点</a-button>
     <a-input v-model:value="endValue" style="width: calc(10% - 10px);margin-right:10px;"></a-input>
-    <a-input v-if="false" v-model:value="editName" style="width: calc(15% - 10px);margin-right:10px;" placeholder="文件命名"></a-input>
     <a-button type="primary" :style="{width: 'calc(10% - 10px)',marginRight: '10px'}" @click="cutVideo">开始截取视频</a-button>
     <a-button type="primary" :style="{width: 'calc(10% - 10px)',marginRight: '10px'}" @click="cutAudio">开始截取音频</a-button>
     <a-button type="primary" :style="{width: '10%'}" @click="viewNote">查看记录</a-button>
   </div>
+  <div :style="{width: widthRate + '%',marginTop: '10px'}">
+    <a-button type="primary" :style="{width: 'calc(10% - 10px)',marginRight: '10px'}" @click="startRecord">开启录频</a-button>
+    <a-button type="primary" :style="{width: 'calc(10% - 10px)',marginRight: '10px'}" @click="stopRecord">关闭录频</a-button>
+    <a-button type="primary" :style="{width: 'calc(10% - 10px)',marginRight: '10px'}" @click="showRecord">查看录频记录</a-button>
+    <a-button type="primary" :style="{width: 'calc(10% - 10px)',marginRight: '10px'}" @click="mergeRecord">合并视频</a-button>
+  </div>
 </div>
 
   <a-drawer
+      :width="520"
       v-model:open="visible"
       class="custom-class"
       title="历史剪辑"
       placement="right"
   >
     <div v-for="file in editVideoList">
+      <figure>
+        <figcaption><a :href="apiPref + file.url" :download="file.name">{{file.name}}</a></figcaption>
+        <figcaption>{{file.time}}</figcaption>
+        <video v-if="file.url.endsWith('.mp4')" style="width: 90%" controls :src="apiPref + file.url"></video>
+        <audio v-if="file.url.endsWith('.mp3')" style="width: 90%" controls :src="apiPref + file.url"></audio>
+        <audio v-if="file.url.endsWith('.m4a')" style="width: 90%" controls :src="apiPref + file.url"></audio>
+        <audio v-if="file.url.endsWith('.wav')" style="width: 90%" controls :src="apiPref + file.url"></audio>
+      </figure>
+    </div>
+  </a-drawer>
+
+  <a-drawer
+      :width="520"
+      v-model:open="recordVisible"
+      class="custom-class"
+      title="历史剪辑"
+      placement="right"
+  >
+    <div v-for="file in recordVideoList">
       <figure>
         <figcaption><a :href="apiPref + file.url" :download="file.name">{{file.name}}</a></figcaption>
         <figcaption>{{file.time}}</figcaption>
